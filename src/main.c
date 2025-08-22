b7eb9edf9e0911f129a7d23ab537b2b838ca054b
Kouta Sakatsume <kosakats@c6r1s12.42tokyo.jp>
2025-08-22 16:39:46 +0900

    add_argument_check

--- src/main.c
+++ src/main.c
@@ -3,65 +3,59 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kotasakatsume <kotasakatsume@student.42    +#+  +:+       +#+        */
+/*   By: kosakats <kosakats@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/16 13:03:06 by kotasakatsu       #+#    #+#             */
-/*   Updated: 2025/08/20 19:54:54 by kotasakatsu      ###   ########.fr       */
+/*   Updated: 2025/08/22 12:33:11 by kosakats         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub.h"
 
-void error_exit(char *message)
+void	error_exit(char *message)
 {
-    write(2, "Error\n", 6);
-    if (message)
-        write(2, message, strlen(message));
-    exit(1);
+	write(2, "Error\n", 6);
+	if (message)
+		write(2, message, strlen(message));
+	exit(1);
 }
 
-int check_extension(const char *filename, const char *extension)
+int	check_extension(const char *filename, const char *extension)
 {
-	(void)extension;
-	printf("test:%s\n", filename);
-	
-	
-	return(1);
+	size_t	len;
 
+	if (!filename)
+		return (0);
+	len = strlen(filename);
+	if (len < 4 || strncmp(filename + len - 4, extension, 5))
+		return (0);
+	return (1);
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    t_game  game;
-	(void)argv; // 引数は使用しない場合の警告回避
+	t_game	game;
 
-
-    // 1. 引数のチェック
-    if (argc != 2)
-        return (error_exit("Usage: ./cub3d <map.cub>\n"), 1);
-    if (!check_extension(argv[1], ".cub"))
-        return (error_exit("File must have .cub extension\n"), 1);
-	
-	//2.構造体初期化
+	if (argc != 2)
+		return (error_exit("Usage: ./cub3d <map.cub>\n"), 1);
+	if (!check_extension(argv[1], ".cub"))
+		return (error_exit("File must have .cub extension\n"), 1);
+	// 2.構造体初期化
 	init_game(&game);
-	
-    // 3. パース処理の開始
-    // if (!parse_file(&game, argv[1]))
-    //     return (1); // parse_file内でエラーメッセージは出力済み
-
-    // 4. ゲームの初期化と実行
-    // init_game(&game);
-    // run_game(&game);
-    
-    return (0);
+	// 3. パース処理の開始
+	// if (!parse_file(&game, argv[1]))
+	//     return (1); // parse_file内でエラーメッセージは出力済み
+	// 4. ゲームの初期化と実行
+	// init_game(&game);
+	// run_game(&game);
+	return (0);
 }
-
 
 //以下、mkuidaの記述部分
 // int main()
 // {
 // 	t_game game;
-	
+
 // 	//test条件設定
 // 	if(set_test(&game) == 1)
 // 	{
