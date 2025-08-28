b7aaa4ddc91af8a5a16c7528a2a7f4d3f8c4d0d5
reprise39 <reprise39@yahoo.co.jp>
2025-08-28 15:35:20 +0900

    dda

--- src/raycaster/dda.c
+++ src/raycaster/dda.c
@@ -6,17 +6,22 @@
 /*   By: mkuida <reprise39@yahoo.co.jp>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/27 12:39:29 by mkuida            #+#    #+#             */
-/*   Updated: 2025/08/28 15:11:01 by mkuida           ###   ########.fr       */
+/*   Updated: 2025/08/28 15:30:22 by mkuida           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub.h"
 
-static bool	check_out_map(t_map *map, int X, int Y)
+static bool	check_out_map(t_map *map, t_linedraw *line)
 {
-	if (X < -1 || Y < -1)
+	int x;
+	int y;
+
+	x = (line->tileX);
+	y = (line->tileX);
+	if (x < -1 || y < -1)
 		return (true);
-	if (X > (map->width) || Y > (map->height))
+	if (x > (map->width) || y > (map->height))
 		return (true);
 	return (false);
 }
@@ -26,6 +31,14 @@ static bool	check_hit_wall(t_map *map, int X, int Y)
 	if (map->map[Y][X] == '1')
 		return (true);
 	return (false);
+}
+
+static void	out_map(t_game *game, t_linedraw *line)
+{
+	(void) *game;
+	printf("error : out  map\n");
+	printf("(line->tileX) = %d , (line->tileY) = %d\n", (line->tileX), (line->tileY));
+	exit(1);
 }
 
 void	dda(t_linedraw *line, t_game *game)
@@ -47,14 +60,8 @@ void	dda(t_linedraw *line, t_game *game)
 			(line->tileY) += (line->stepY);
 			(line->hit_side) = 1;
 		}
-		if (check_out_map(&(game->map_data), (line->tileX),
-				(line->tileY)) == true)
-		{
-			printf("out map\n");
-			printf("(line->tileX) = %d , (line->tileY) = %d\n", (line->tileX),
-				(line->tileY));
-			exit(1);
-		}
+		if (check_out_map(&(game->map_data), line) == true)
+			out_map(game,line);
 		if (check_hit_wall(&(game->map_data), (line->tileX),
 				(line->tileY)) == true)
 			hit = 1;
